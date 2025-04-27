287b59e206e97a1ca3910870cf113709df60cfac
David <97574531+DavidHCM@users.noreply.github.com>
2025-04-27 13:40:05 -0600

    changes to workflow and prettier

--- src/middlewares/authenticationMiddleware.ts
+++ src/middlewares/authenticationMiddleware.ts
@@ -16,7 +16,9 @@ export const authenticate = (
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    logger.warn(`Authentication failed: No token provided. URL: ${req.originalUrl}`);
+    logger.warn(
+      `Authentication failed: No token provided. URL: ${req.originalUrl}`,
+    );
     throw new Error("Authentication required: " + HTTP_STATUS.AUTH_ERROR);
   }
 
@@ -24,14 +26,20 @@ export const authenticate = (
     const decoded = jwt.verify(token, secretKey);
     if (typeof decoded === "object" && decoded !== null) {
       req.user = decoded as User;
-      logger.info(`Authentication successful for user: ${JSON.stringify(req.user)}. URL: ${req.originalUrl}`);
+      logger.info(
+        `Authentication successful for user: ${JSON.stringify(req.user)}. URL: ${req.originalUrl}`,
+      );
     } else {
-      logger.warn(`Authentication failed: Invalid token format. URL: ${req.originalUrl}`);
+      logger.warn(
+        `Authentication failed: Invalid token format. URL: ${req.originalUrl}`,
+      );
       throw new Error("Invalid token: " + HTTP_STATUS.FORBIDDEN);
     }
     next();
   } catch (error) {
-    logger.error(`Authentication failed: Invalid token. URL: ${req.originalUrl}. Error: ${error}`);
+    logger.error(
+      `Authentication failed: Invalid token. URL: ${req.originalUrl}. Error: ${error}`,
+    );
     throw new Error("Invalid token: " + HTTP_STATUS.FORBIDDEN);
   }
 };
